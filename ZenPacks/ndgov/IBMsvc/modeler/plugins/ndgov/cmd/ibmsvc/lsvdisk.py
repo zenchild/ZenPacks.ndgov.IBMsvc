2cfc4731fd72c2f9e6e923545f7367f97e7b8fa5
Zenoss Account <zenoss@itdzenosstst1.itd.nd.gov>
2009-06-22 16:40:09 -0500

    Modified modeler code ... still not working right

--- ZenPacks/ndgov/IBMsvc/modeler/plugins/ndgov/cmd/ibmsvc/lsvdisk.py
+++ ZenPacks/ndgov/IBMsvc/modeler/plugins/ndgov/cmd/ibmsvc/lsvdisk.py
@@ -14,22 +14,31 @@ class lsvdisk(CommandPlugin):
     modname = "ZenPacks.ndgov.IBMsvc.SVCvdisk"
 
     def process(self, device, results, log):
-	log.info('Results from command %s' % results)
-        rm = self.relMap()
+	#log.info('Results from command %s' % results)
+        #rm = self.relMap()
+        rm = {}
+        print "RELMAP: %s" % rm
         results = results.strip()
 	vdisks = results.split("\n")
         bline = ""
         log.debug("=========================")
-        log.debug("TYPE: %s" % type(rm))
-        log.debug("MAPS: %s" % rm.maps)
-        log.debug("COMPNAME: %s" % rm.compname )
-        log.debug("RM: %s" % dir(rm) )
+        #log.debug("TYPE: %s" % type(rm))
+        #log.debug("MAPS: %s" % rm.maps)
+        #log.debug("COMPNAME: %s" % rm.compname )
+        #log.debug("Dev: %s" % dir(device) )
+        #log.debug("Dev2: %s" % device )
+        #log.debug("DevId: %s" % device.id )
+        #log.debug("SELF: %s" % type(self) )
+        #log.debug("SELF DIR: %s" % dir(self) )
         log.debug("=========================")
         for vdisk in vdisks:
             om = self.objectMap()
             vdisk = vdisk.strip()
             vdisk_a = vdisk.split(':')
             om.id = self.prepId(vdisk_a[0])
+            if not rm.has_key(vdisk_a[5]):
+               rm[vdisk_a[5]] = []
+
             #id:name:IO_group_id:IO_group_name:status:mdisk_grp_id:mdisk_grp_name:capacity:type:FC_id:FC_name:RC_id:RC_name:vdisk_UID:fc_map_count:copy_count:fast_write_state
             #log.debug("VDISK 1: %s" % vdisk_a[1] )
             #log.debug("VDISK 2: %s" % vdisk_a[2])
@@ -38,6 +47,7 @@ class lsvdisk(CommandPlugin):
             #log.debug("VDISK 7: %s" % vdisk_a[7])
             #log.debug("VDISK 13: %s" % vdisk_a[13])
             (om.name, om.io_group_id, om.status, om.mdisk_grp_id, om.capacity, om.vdisk_UID) = vdisk_a[1],vdisk_a[2],vdisk_a[4],vdisk_a[5],vdisk_a[7],vdisk_a[13]
-            rm.append(om)
+            rm[vdisk_a[5]].append(om)
+
         return [rm]
 
