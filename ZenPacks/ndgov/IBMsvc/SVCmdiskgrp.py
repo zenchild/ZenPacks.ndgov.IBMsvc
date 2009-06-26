4951660bc37ef87f04bd60675bb595eb84dd13f6
Dan Wanek <dan.wanek@gmail.com>
2009-06-26 14:31:51 -0500

    Multilevel relations is finally working!!!

--- ZenPacks/ndgov/IBMsvc/SVCmdiskgrp.py
+++ ZenPacks/ndgov/IBMsvc/SVCmdiskgrp.py
@@ -38,75 +38,69 @@ from Products.ZenModel.DeviceComponent import DeviceComponent
 from Products.ZenModel.ManagedEntity import ManagedEntity
 from Products.ZenUtils.Utils import prepId
 
+import logging
 log = logging.getLogger("zen.SVCmdiskgrp")
+
+from ZenPacks.ndgov.IBMsvc.SVCvdisk import manage_addvDisk
 
 
 class SVCmdiskgrp(DeviceComponent, ManagedEntity):
-    """IBM San Volume Controller(SVC) mDisk Group"""
+	"""IBM San Volume Controller(SVC) mDisk Group"""
+	portal_type = meta_type = 'SVCmdiskgrp'
+    
+	#**************Custom data Variables here from modeling************************
 
-    portal_type = meta_type = 'SVCmdiskgrp'
-    
-    #**************Custom data Variables here from modeling************************
-    
-    mdiskgrp_name = ""
-    status = ""
-    mdisk_count = -1
-    vdisk_count = -1
-    free_capacity = -1
-    capacity = -1
-    
-    #**************END CUSTOM VARIABLES *****************************
-    
-    
-    #*************  Those should match this list below *******************
-    _properties = (
-        {'id':'mdiskgrp_name', 'type':'string', 'mode':''},
-        {'id':'status', 'type':'string', 'mode':''},
-        {'id':'mdisk_count', 'type':'int', 'mode':''},
-        {'id':'vdisk_count', 'type':'int', 'mode':''},
-        {'id':'capacity', 'type':'int', 'mode':''},
-        {'id':'free_capacity', 'type':'int', 'mode':''},
-        )
-    #****************
-    
-    _relations = (
-        ("cluster", ToOne(ToManyCont,
-            'ZenPacks.ndgov.IBMsvc.SVCcluster', 'mdiskgrp')),
-        ("vdisk", ToManyCont(ToOne,
-            'ZenPacks.ndgov.IBMsvc.SVCvdisk', 'mdiskgrp')),
-        )
+	mdiskgrp_name = ""
+	status = ""
+	mdisk_count = -1
+	vdisk_count = -1
+	free_capacity = -1
+	capacity = -1
 
-    factory_type_information = ( 
-        { 
-            'id'             : 'SVCmdiskgrp',
-            'meta_type'      : 'SVCmdiskgrp',
-            'description'    : """SAN Volume Controller mDisk Group""",
-            'icon'           : 'SanInterface_icon.gif',
-            'product'        : 'ZenPacks.ndgov.IBMsvc',
-            'factory'        : 'manage_addSVCmdiskgrp',
-            'immediate_view' : 'viewSVCmdiskgrp',
-            'actions'        :
-            ( 
-                { 'id'            : 'status'
-                , 'name'          : 'Status'
-                , 'action'        : 'viewSVCmdiskgrp'
-                , 'permissions'   : (ZEN_VIEW, )
-                },
-                { 'id'            : 'perfConf'
-                , 'name'          : 'Template'
-                , 'action'        : 'objTemplates'
-                , 'permissions'   : (ZEN_CHANGE_SETTINGS, )
-                },                
-                { 'id'            : 'viewHistory'
-                , 'name'          : 'Modifications'
-                , 'action'        : 'viewHistory'
-                , 'permissions'   : (ZEN_VIEW, )
-                },
-            )
-          },
-        ) 
+	#**************END CUSTOM VARIABLES *****************************
 
-
+	#*************  Those should match this list below *******************
+	_properties = (
+			{'id':'mdiskgrp_name', 'type':'string', 'mode':''},
+			{'id':'status', 'type':'string', 'mode':''},
+			{'id':'mdisk_count', 'type':'int', 'mode':''},
+			{'id':'vdisk_count', 'type':'int', 'mode':''},
+			{'id':'capacity', 'type':'int', 'mode':''},
+			{'id':'free_capacity', 'type':'int', 'mode':''},
+			)
+	#****************
+	_relations = (
+			("cluster", ToOne(ToManyCont, 'ZenPacks.ndgov.IBMsvc.SVCcluster', 'mdiskgrp')),
+			("vdisk", ToManyCont(ToOne, 'ZenPacks.ndgov.IBMsvc.SVCvdisk', 'mdiskgrp')),
+			)
+	
+	factory_type_information = (
+			{
+				'id'             : 'SVCmdiskgrp',
+				'meta_type'      : 'SVCmdiskgrp',
+				'description'    : """SAN Volume Controller mDisk Group""",
+				'icon'           : 'SanInterface_icon.gif',
+				'product'        : 'ZenPacks.ndgov.IBMsvc',
+				'factory'        : 'manage_addSVCmdiskgrp',
+				'immediate_view' : 'viewSVCmdiskgrp',
+				'actions'        :
+				(
+					{ 'id'            : 'status' , 'name'          : 'Status'
+						, 'action'        : 'viewSVCmdiskgrp'
+						, 'permissions'   : (ZEN_VIEW, )
+					},
+					{ 'id'            : 'perfConf'
+						, 'name'          : 'Template'
+						, 'action'        : 'objTemplates'
+						, 'permissions'   : (ZEN_CHANGE_SETTINGS, )
+					},
+					{ 'id'            : 'viewHistory'
+						, 'name'          : 'Modifications'
+						, 'action'        : 'viewHistory'
+						, 'permissions'   : (ZEN_VIEW, )
+					},
+				)
+			},) 
 
 	# ============= Special modeling methods ============= 
 	"""
@@ -116,45 +110,45 @@ class SVCmdiskgrp(DeviceComponent, ManagedEntity):
 	showing me the way.
 	"""
 
+	"""
 	#def modelMdisks(self, mdisk_hash):
-		"""
-		This method creates SVCmdisk objects that are mapped
-		to the correct mDiskgrp object.
-		"""
+		#This method creates SVCmdisk objects that are mapped
+		#to the correct mDiskgrp object.
+	"""
 
 
 	def modelVdisks(self, vdisk_hash):
-        """
+		"""
 		This method creates SVCvdisk objects that are mapped
 		to the correct mDiskgrp object.
-        """
+		"""
 		for vdisk_id in vdisk_hash.keys():
 			vdisk = manage_addvDisk(self.vdisk, vdisk_id)
-			for vdisk_attr in vdisk_hash[vdisk_id]
+			for vdisk_attr in vdisk_hash[vdisk_id]:
 				log.debug("vDisk Key: %s -> data: %s" %( vdisk_attr, vdisk_hash[vdisk_id][vdisk_attr]))
-                attrib = getattr(vdisk, vdisk_attr)
-                if callable( attrib ):
-                    attrib(vdisk_hash[vdisk_id][vdisk_attr])
-                else:
-                    setattr(vdisk, vdisk_attr, vdisk_hash[vdisk_id][vdisk_attr])
+				attrib = getattr(vdisk, vdisk_attr)
+				if callable( attrib ):
+					attrib(vdisk_hash[vdisk_id][vdisk_attr])
+				else:
+					setattr(vdisk, vdisk_attr, vdisk_hash[vdisk_id][vdisk_attr])
 
 
 	# =========== End Special modeling methods =========== 
 
 
-    def device(self): return self.cluster()
-    
-    #THIS FUNCTION IS REQUIRED LEAVE IT BE IF NO RRD INFO IS PRESENT	    
-    def getRRDNames(self):
+	def device(self): return self.cluster()
+		
+	#THIS FUNCTION IS REQUIRED LEAVE IT BE IF NO RRD INFO IS PRESENT
+	def getRRDNames(self):
 		return ['']
-
-    def getRRDTemplates(self):
-        """
-        """
-        default = self.getRRDTemplateByName("SVC_mdiskgrp")
-        if default:
-            return [default]
-        return []
+	
+	def getRRDTemplates(self):
+		"""
+		"""
+		default = self.getRRDTemplateByName("SVC_mdiskgrp")
+		if default:
+			return [default]
+		return []
 
 
 InitializeClass(SVCmdiskgrp)
--- ZenPacks/ndgov/IBMsvc/modeler/plugins/ndgov/cmd/ibmsvc/SVCcomponentsMap.py
+++ ZenPacks/ndgov/IBMsvc/modeler/plugins/ndgov/cmd/ibmsvc/SVCcomponentsMap.py
@@ -20,18 +20,20 @@
 __doc__ = """SVCcomponentMap Collects information related to a SVC mdisk group.
 """
 
-import re from Products.DataCollector.plugins.CollectorPlugin import CommandPlugin
+import re
+from Products.DataCollector.plugins.CollectorPlugin import CommandPlugin
+from Products.DataCollector.plugins.DataMaps import MultiArgs
 
 #def parseMdiskInfo(mdisk_a):
-	"""
-	Parse mDisk info out of command and return a hash like so:
-	{'mdiskgrp_id' : { 'mdisk_id' : { .... mdisk info .... }}}
-	----------------------------------------------------------
-	*** svcinfo lsmdisk header fields ***
-	0-4 = id:name:status:mode:mdisk_grp_id:
-	5-9 = mdisk_grp_name:capacity:ctrl_LUN_#:controller_name:UID
-	----------------------------------------------------------
-	"""
+#	"""
+#	Parse mDisk info out of command and return a hash like so:
+#	{'mdiskgrp_id' : { 'mdisk_id' : { .... mdisk info .... }}}
+#	----------------------------------------------------------
+#	*** svcinfo lsmdisk header fields ***
+#	0-4 = id:name:status:mode:mdisk_grp_id:
+#	5-9 = mdisk_grp_name:capacity:ctrl_LUN_#:controller_name:UID
+#	----------------------------------------------------------
+#	"""
 
 def parseVdiskInfo(vdisk_a):
 	"""
@@ -53,13 +55,15 @@ def parseVdiskInfo(vdisk_a):
 
 	for vdisk in vdisk_a:
 		vdata = vdisk.split(':')
+		if vdata[5] == 'many':
+			continue
 		vdata_h = { 'name': vdata[1],
 			'io_group_id': vdata[2],
 			'status': vdata[4],
 			'mdiskgrp_id': vdata[5],
 			'vdisk_UID': vdata[13]}
 
-		if not vdisk_h.has_key('vdata[5]'):
+		if not vdisk_h.has_key(vdata[5]):
 			vdisk_h[vdata[5]] = {}
 
 		vdisk_h[vdata[5]][vdata[0]] = vdata_h
@@ -67,7 +71,8 @@ def parseVdiskInfo(vdisk_a):
 	return vdisk_h
 
 
-class SVCcomponentMap(CommandPlugin):
+class SVCcomponentsMap(CommandPlugin):
+
 	"""
 	Run:
 	svcinfo lsmdiskgrp -bytes -nohdr -delim :
@@ -86,7 +91,7 @@ class SVCcomponentMap(CommandPlugin):
 	modname = "ZenPacks.ndgov.IBMsvc.SVCmdiskgrp"
 	
 	def process(self, device, results, log):
-		log.info('Results from command %s' % results)
+		#log.info('Results from command %s' % results)
 		rm = self.relMap()
 		results = results.strip()
 		(mdiskgrps,mdisks,vdisks) = results.split('__DELIM__')
@@ -99,16 +104,20 @@ class SVCcomponentMap(CommandPlugin):
 
 		#mdisk_h = parseMdiskInfo(mdisks)
 		vdisk_h = parseVdiskInfo(vdisks)
+		#log.debug("VDISK HASH: %s" % vdisk_h)
 
 		for mdg in mdiskgrps:
 			om = self.objectMap()
 			mdg = mdg.strip()
 			mdg_a = mdg.split(':')
 			om.id = self.prepId(mdg_a[0])
-			#id:name:status:mdisk_count:vdisk_count:capacity:extent_size:free_capacity:virtual_capacity:used_capacity:real_capacity:overallocation:warning
 			(om.mdiskgrp_name, om.status, om.mdisk_count, om.vdisk_count, om.capacity,
 					om.free_capacity)=mdg_a[1],mdg_a[2],mdg_a[3],mdg_a[4],mdg_a[5],mdg_a[7]
-			om.modelVdisks = vdisk_h[om.id]
+
+			# It is possible for a mdiskgroup to have no vdisks. Check for it.
+			if vdisk_h.has_key(mdg_a[0]):
+				om.modelVdisks = MultiArgs(vdisk_h[mdg_a[0]])
 			rm.append(om)
+
 
 		return [rm]
